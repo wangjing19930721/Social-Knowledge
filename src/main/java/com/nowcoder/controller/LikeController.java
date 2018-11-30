c767957c8f2cac7ef4bab2ef6890ac38d3e37468
wangjing19930721 <718720336@qq.com>
2018-11-30 15:37:31 +0800

    Update LikeController.java

--- src/main/java/com/nowcoder/controller/LikeController.java
+++ src/main/java/com/nowcoder/controller/LikeController.java
@@ -17,9 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Created by nowcoder on 2016/7/30.
- */
+
 @Controller
 public class LikeController {
     @Autowired
