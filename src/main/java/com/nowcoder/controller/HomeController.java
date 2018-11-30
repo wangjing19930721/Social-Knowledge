14f56f7ee4f528d37a50d652687c6e6f18cb7bc7
wangjing19930721 <718720336@qq.com>
2018-11-30 15:36:43 +0800

    Update HomeController.java

--- src/main/java/com/nowcoder/controller/HomeController.java
+++ src/main/java/com/nowcoder/controller/HomeController.java
@@ -18,9 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by nowcoder on 2016/7/15.
- */
+
 @Controller
 public class HomeController {
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
