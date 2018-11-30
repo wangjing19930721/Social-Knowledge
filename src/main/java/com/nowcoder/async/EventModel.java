19737ceca9ba6466d4839c5e136b1b7ab59dae11
wangjing19930721 <718720336@qq.com>
2018-11-30 15:30:58 +0800

    Update EventModel.java

--- src/main/java/com/nowcoder/async/EventModel.java
+++ src/main/java/com/nowcoder/async/EventModel.java
@@ -3,9 +3,7 @@ package com.nowcoder.async;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by nowcoder on 2016/7/30.
- */
+
 public class EventModel {
     private EventType type;
     private int actorId;
