f9a391cf81d09f821da52d0fee3667187e82d8a7
wangjing19930721 <718720336@qq.com>
2018-11-30 15:49:57 +0800

    Update Feed.java

--- src/main/java/com/nowcoder/model/Feed.java
+++ src/main/java/com/nowcoder/model/Feed.java
@@ -4,11 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 import java.util.Date;
-import java.util.Map;
 
-/**
- * Created by nowcoder on 2016/8/12.
- */
 public class Feed {
     private int id;
     private int type;
