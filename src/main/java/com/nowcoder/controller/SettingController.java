052ad5fc28cb18aa876274c922400e12de495423
wangjing19930721 <718720336@qq.com>
2018-11-30 15:39:21 +0800

    Update SettingController.java

--- src/main/java/com/nowcoder/controller/SettingController.java
+++ src/main/java/com/nowcoder/controller/SettingController.java
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
-/**
- * Created by nowcoder on 2016/7/10.
- */
+
 @Controller
 public class SettingController {
     @Autowired
