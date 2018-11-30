013683052ae712fbc8bc57d2433239f0a40ea7ed
wangjing19930721 <718720336@qq.com>
2018-11-30 15:44:27 +0800

    Update LoginTicketDAO.java

--- src/main/java/com/nowcoder/dao/LoginTicketDAO.java
+++ src/main/java/com/nowcoder/dao/LoginTicketDAO.java
@@ -3,9 +3,7 @@ package com.nowcoder.dao;
 import com.nowcoder.model.LoginTicket;
 import org.apache.ibatis.annotations.*;
 
-/**
- * Created by nowcoder on 2016/7/2.
- */
+
 @Mapper
 public interface LoginTicketDAO {
     String TABLE_NAME = "login_ticket";
