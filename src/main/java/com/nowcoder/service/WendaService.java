fb42a765f31907cb24f5799bc69de864f9fc6ef6
wangjing19930721 <718720336@qq.com>
2018-11-30 16:15:46 +0800

    Update WendaService.java

--- src/main/java/com/nowcoder/service/WendaService.java
+++ src/main/java/com/nowcoder/service/WendaService.java
@@ -2,9 +2,7 @@ package com.nowcoder.service;
 
 import org.springframework.stereotype.Service;
 
-/**
- * Created by nowcoder on 2016/7/10.
- */
+
 @Service
 public class WendaService {
     public String getMessage(int userId) {
