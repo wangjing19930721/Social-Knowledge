cb3d741e3864c2cf45405d52fb8c529fdc95eb9b
wangjing19930721 <718720336@qq.com>
2018-11-30 16:16:23 +0800

    Update JedisAdapter.java

--- src/main/java/com/nowcoder/util/JedisAdapter.java
+++ src/main/java/com/nowcoder/util/JedisAdapter.java
@@ -15,9 +15,7 @@ import java.util.List;
 import java.util.Set;
 
 
-/**
- * Created by nowcoder on 2016/7/30.
- */
+
 @Service
 public class JedisAdapter implements InitializingBean {
     private static final Logger logger = LoggerFactory.getLogger(JedisAdapter.class);
